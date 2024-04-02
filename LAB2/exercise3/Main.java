f526981e5b4b3de6a0707f2c5daab6de4bec6b76
theEMA <61084889+theEMA-dev@users.noreply.github.com>
2024-04-02 21:20:43 +0300

    Fix array swap bug and handle invalid indices

--- LAB2/exercise3/Main.java
+++ LAB2/exercise3/Main.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 
 public class Main {
   public static <T> void swap(T[] array, int index1, int index2) {
-    T temp = array[index1];
-    array[index1] = array[index2];
-    array[index2] = temp;
+    if (index1 < 0 || index1 >= array.length || index2 < 0 || index2 >= array.length) {
+      System.out.println("Invalid indices, cannot perform swap");
+    } else {
+      T temp = array[index1];
+      array[index1] = array[index2];
+      array[index2] = temp;
+    }
   }
 
   public static void main(String[] args) {
@@ -19,13 +23,12 @@ public class Main {
     System.out.println("String array: " + Arrays.toString(stringArray));
     System.out.println("Double array: " + Arrays.toString(doubleArray));
 
-    swap(intArray, 1, 3);
-    swap(stringArray, 0, 2);
-    swap(doubleArray, 2, 3);
-
     System.out.println("\n-- After swapping --");
+    swap(intArray, 1, 3);
     System.out.println("Integer array: " + Arrays.toString(intArray));
+    swap(stringArray, -4, 2);
     System.out.println("String array: " + Arrays.toString(stringArray));
+    swap(doubleArray, 2, 3);
     System.out.println("Double array: " + Arrays.toString(doubleArray));
   }
 }
